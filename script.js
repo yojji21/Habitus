93c7491aaf3638b170dc126eca1ecdd9f67de793
yojji21 <luizmigueldeoliveira100@hotmail.com>
2024-07-29 22:02:37 -0300

    subindo no git

---
+++ script.js
@@ -0,0 +1,258 @@
+
+const html = document.querySelector("html")
+
+const cards = document.querySelector(".cards")
+const modalitiesSection = document.querySelector(".modalities")
+const card1 = document.querySelector("#card1")
+const card2 = document.querySelector("#card2")
+const card3 = document.querySelector("#card3")
+const card4 = document.querySelector("#card4")
+
+card1.addEventListener("click", () => {
+    const h1 = card1.querySelector("h1")
+    const description = "Musculação é uma prática de exercícios físicos que utiliza pesos e equipamentos específicos para fortalecer e desenvolver a musculatura. Envolve séries e repetições de movimentos controlados, visando aumentar a força, resistência e hipertrofia muscular."
+
+
+    const desc = document.createElement("p")
+    desc.textContent = description
+
+    const descBox = document.createElement("div")
+    descBox.classList.add("desc_box")
+
+    const unClickableBack = document.createElement("div")
+    unClickableBack.classList.add("unclickabe")
+
+    
+    const exitButton = document.createElement("button")
+    exitButton.innerHTML = `
+    <span>Fechar</span>
+    `
+
+    setTimeout(() => {
+        descBox.style.width = "80rem"
+        descBox.style.height = "70rem"
+        descBox.style.opacity = "1"
+    }, 1)
+
+    setTimeout(() => {
+        title.style.opacity = "1"
+        desc.style.opacity = "1"
+        exitButton.style.opacity = "1"
+    }, 210)
+
+    
+
+    const title = document.createElement("h1")
+
+    title.textContent = h1.textContent
+
+    html.appendChild(unClickableBack)
+
+    cards.appendChild(descBox)
+
+    descBox.appendChild(title)
+    descBox.appendChild(desc)
+    descBox.appendChild(exitButton)
+
+    exitButton.addEventListener("click", () => {
+
+        descBox.style.width = "5rem"
+        descBox.style.height = "5rem"
+        descBox.style.opacity = "0"
+
+        title.style.display = "none"
+        desc.style.display = "none"
+        exitButton.style.display = "none"
+
+        setTimeout(() => {
+            unClickableBack.remove();
+            descBox.remove();
+        }, 200)
+    })
+})
+
+card2.addEventListener("click", () => {
+    const h1 = card2.querySelector("h1")
+    const description = "O spinning é uma modalidade de exercício aeróbico realizada em bicicletas estacionárias, frequentemente em grupo e acompanhada por música motivacional. As aulas são guiadas por um instrutor que varia a intensidade, simulando subidas, descidas e sprints. É eficaz para melhorar a resistência cardiovascular, queimar calorias e fortalecer os músculos das pernas."
+
+
+    const desc = document.createElement("p")
+    desc.textContent = description
+
+    const descBox = document.createElement("div")
+    descBox.classList.add("desc_box")
+
+    const unClickableBack = document.createElement("div")
+    unClickableBack.classList.add("unclickabe")
+
+    const exitButton = document.createElement("button")
+    exitButton.innerHTML = `
+    <span>Fechar</span>
+    `
+
+    setTimeout(() => {
+        descBox.style.width = "80rem"
+        descBox.style.height = "70rem"
+        descBox.style.opacity = "1"
+    }, 1)
+
+    setTimeout(() => {
+        title.style.opacity = "1"
+        desc.style.opacity = "1"
+        exitButton.style.opacity = "1"
+    }, 210)
+
+    
+
+    const title = document.createElement("h1")
+
+    title.textContent = h1.textContent
+
+    html.appendChild(unClickableBack)
+
+    cards.appendChild(descBox)
+
+    descBox.appendChild(title)
+    descBox.appendChild(desc)
+    descBox.appendChild(exitButton)
+
+    exitButton.addEventListener("click", () => {
+
+        descBox.style.width = "5rem"
+        descBox.style.height = "5rem"
+        descBox.style.opacity = "0"
+
+        title.style.display = "none"
+        desc.style.display = "none"
+        exitButton.style.display = "none"
+
+        setTimeout(() => {
+            unClickableBack.remove();
+            descBox.remove();
+        }, 200)
+    })
+})
+
+card3.addEventListener("click", () => {
+    const h1 = card3.querySelector("h1")
+    const description = "FitDance é uma modalidade de dança fitness que combina diversos estilos musicais e coreografias dinâmicas para proporcionar um treino divertido e eficaz. As aulas são realizadas em grupo, seguindo a orientação de um instrutor, promovendo a queima de calorias, a melhora da coordenação motora e o bem-estar geral. É uma atividade inclusiva, voltada para pessoas de todas as idades e níveis de condicionamento físico."
+
+
+    const desc = document.createElement("p")
+    desc.textContent = description
+
+    const descBox = document.createElement("div")
+    descBox.classList.add("desc_box")
+
+    const unClickableBack = document.createElement("div")
+    unClickableBack.classList.add("unclickabe")
+
+    const exitButton = document.createElement("button")
+    exitButton.innerHTML = `
+    <span>Fechar</span>
+    `
+
+    setTimeout(() => {
+        descBox.style.width = "80rem"
+        descBox.style.height = "70rem"
+        descBox.style.opacity = "1"
+    }, 1)
+
+    setTimeout(() => {
+        title.style.opacity = "1"
+        desc.style.opacity = "1"
+        exitButton.style.opacity = "1"
+    }, 210)
+
+    
+
+    const title = document.createElement("h1")
+
+    title.textContent = h1.textContent
+
+    html.appendChild(unClickableBack)
+
+    cards.appendChild(descBox)
+
+    descBox.appendChild(title)
+    descBox.appendChild(desc)
+    descBox.appendChild(exitButton)
+
+    exitButton.addEventListener("click", () => {
+
+        descBox.style.width = "5rem"
+        descBox.style.height = "5rem"
+        descBox.style.opacity = "0"
+
+        title.style.display = "none"
+        desc.style.display = "none"
+        exitButton.style.display = "none"
+
+        setTimeout(() => {
+            unClickableBack.remove();
+            descBox.remove();
+        }, 200)
+    })
+})
+
+card4.addEventListener("click", () => {
+    const h1 = card4.querySelector("h1")
+    const description = "A Academia Habitus, tem muito a oferecer, desde equipamentos de primeira à professores incríveis! Nosso principal objetivo é fornecer à você não só um treino, e sim uma experiência. Vamos-lá! Nunca é tarde para começar!"
+
+
+    const desc = document.createElement("p")
+    desc.textContent = description
+
+    const descBox = document.createElement("div")
+    descBox.classList.add("desc_box")
+
+    const unClickableBack = document.createElement("div")
+    unClickableBack.classList.add("unclickabe")
+
+    const exitButton = document.createElement("button")
+    exitButton.innerHTML = `
+    <span>Fechar</span>
+    `
+
+    setTimeout(() => {
+        descBox.style.width = "80rem"
+        descBox.style.height = "70rem"
+        descBox.style.opacity = "1"
+    }, 1)
+
+    setTimeout(() => {
+        title.style.opacity = "1"
+        desc.style.opacity = "1"
+        exitButton.style.opacity = "1"
+    }, 210)
+
+    
+
+    const title = document.createElement("h1")
+
+    title.textContent = h1.textContent
+
+    html.appendChild(unClickableBack)
+
+    cards.appendChild(descBox)
+
+    descBox.appendChild(title)
+    descBox.appendChild(desc)
+    descBox.appendChild(exitButton)
+
+    exitButton.addEventListener("click", () => {
+
+        descBox.style.width = "5rem"
+        descBox.style.height = "5rem"
+        descBox.style.opacity = "0"
+
+        title.style.display = "none"
+        desc.style.display = "none"
+        exitButton.style.display = "none"
+
+        setTimeout(() => {
+            unClickableBack.remove();
+            descBox.remove();
+        }, 200)
+    })
+})
